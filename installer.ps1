1affc7f7e6f65a30d61f520b2773a24c84750c50
bobo347844 <bobo347844@users.noreply.github.com>
2018-10-05 08:08:23 +1000

    Dependency Installer
    Flask is only needed by the HTML server, which also does not need the other dependencies.

---
+++ installer.ps1
@@ -0,0 +1,15 @@
+#installer package for Proximity Occupation Sensor
+#Daniel Osmond 13197963
+# 5/10/18
+
+#aws iot sdk for python
+pip2.7 install AWSIoTPythonSDK
+
+#json pickler
+pip2.7 install jsonpickle
+
+#html api
+pip2.7 install Flask
+
+#aws class 3 certificate
+Invoke-WebRequest -Uri https://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem -OutFile root-CA.crt
