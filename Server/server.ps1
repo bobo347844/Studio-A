0434816a0d258135a4b43bc77bc7bd35f7193922
bobo347844 <bobo347844@users.noreply.github.com>
2018-10-05 08:02:56 +1000

    Python2.7
    The json encoder only runs on python2.7, force to start on 2.7 not 3

--- Server/server.ps1
+++ Server/server.ps1
@@ -1,7 +1,7 @@
 #startup script for Proximity Occupation Sensor - Main Server
 # Daniel Osmond
-# 7/08/18
-# Version 1.2
+# 5/10/18
+# Version 1.3
 
 #User Configurable Variables
 $endpoint = "ak3vkd3cgbh1r.iot.us-east-1.amazonaws.com"
@@ -15,5 +15,5 @@ $cert = $certName + "-certificate.pem.crt"
 $privKey = $certName + "-private.pem.key"
 
 #start server
-python server.py -e $endpoint -r $rootCert -c $cert -k $privKey -id $id -t $topic -verbose
+python2.7 server.py -e $endpoint -r $rootCert -c $cert -k $privKey -id $id -t $topic -verbose
 
